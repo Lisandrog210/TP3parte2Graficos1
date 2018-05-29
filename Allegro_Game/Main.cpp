070ef337bcde6f4d301091fd800ae3803fae7a12
Unknown <li_gueva@hotmail.com>
2018-05-29 17:55:28 -0300

    bitmaps assets loading, codigo simplificado

--- Allegro_Game/Main.cpp
+++ Allegro_Game/Main.cpp
@@ -1,57 +1,35 @@
 #include <allegro5\allegro.h>
-
 #include <allegro5\allegro_image.h>
 #include <iostream>
-#include <iostream>
-#include "allegro5/allegro_native_dialog.h"
+#include "allegro5\allegro_native_dialog.h"
 
 using namespace std;
 
 int main(int argc, char **argv) {
 
+
+	
 	ALLEGRO_DISPLAY *display = NULL;
-	ALLEGRO_BITMAP  *sourceImage, *Image = NULL;
+	ALLEGRO_BITMAP  *background, *coolspot, *sierra = NULL;
 
-	if (!al_init()) {
-		al_show_native_message_box(display, "Error", "Error", "Failed to initialize allegro!",
-			NULL, ALLEGRO_MESSAGEBOX_ERROR);
-		return 0;
-	}
-
-	if (!al_init_image_addon()) {
-		al_show_native_message_box(display, "Error", "Error", "Failed to initialize al_init_image_addon!",
-			NULL, ALLEGRO_MESSAGEBOX_ERROR);
-		return 0;
-	}
-
-	display = al_create_display(800, 600);
-
-	if (!display) {
-		al_show_native_message_box(display, "Error", "Error", "Failed to initialize display!",
-			NULL, ALLEGRO_MESSAGEBOX_ERROR);
-		return 0;
-	}
-
-	sourceImage = al_load_bitmap("imagen.jpg");
-	Image = al_create_bitmap(50, 50);
-
-	al_draw_scaled_bitmap
-
-
-	if (!image) {
-		al_show_native_message_box(display, "Error", "Error", "Failed to load image!",
-			NULL, ALLEGRO_MESSAGEBOX_ERROR);
-		al_destroy_display(display);
-		return 0;
-	}
-
-	al_draw_bitmap(image, 50, 50, 0);
-
+	al_init();
+	al_init_image_addon();
+	
+	display = al_create_display(800, 600);	
+	background = al_load_bitmap("meadow.jpg");
+	al_draw_bitmap(background, 0, 0, 0);
 	al_flip_display();
-	al_rest(2);
-	cin.get();
-	al_destroy_display(display);
-	al_destroy_bitmap(image);
+	coolspot = al_load_bitmap("coolspot.png");
+	sierra = al_load_bitmap("sierra.png");
+	al_draw_bitmap(coolspot, 0, 0, 0);
+	al_draw_bitmap(sierra, 100, 100, 0);
+	while (true) {};
 
-	return 0;
+	
+	
+	
+	//al_destroy_display(display);
+	//al_destroy_bitmap(background);
+
+	
 }
