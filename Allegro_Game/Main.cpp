8640f6e7da65700d84af284408f65694fb83db4f
Unknown <li_gueva@hotmail.com>
2018-05-29 17:08:51 -0300

    codigo cambiado en general, orden y loop

--- Allegro_Game/Main.cpp
+++ Allegro_Game/Main.cpp
@@ -1,38 +1,57 @@
 #include <allegro5\allegro.h>
-#include <allegro5\allegro_primitives.h>
+
 #include <allegro5\allegro_image.h>
 #include <iostream>
+#include <iostream>
+#include "allegro5/allegro_native_dialog.h"
+
 using namespace std;
-//--------------Variables--------------
-int displayWidth = 800;
-int displayHeigth = 600;
-//---------------Objects---------------
-ALLEGRO_DISPLAY* display = NULL;
-ALLEGRO_BITMAP* image = NULL;
-
-int init() {
-	if (!al_init()) {
-		cout << "Failed to initialize Allegro" << endl;
-		return 0;
-	}
-	if (!al_init_image_addon()) {
-		cout << "Failled to initialize Allegro's Image Addon" << endl;
-		return 0;
-	}
-
-	display = al_create_display(displayWidth, displayHeigth);
-
-	if (!display) {
-		cout << "Failed to initialize display" << endl;
-		return 0;
-	}
-
-	return 1;
-}
 
 int main(int argc, char **argv) {
-	init();
 
+	ALLEGRO_DISPLAY *display = NULL;
+	ALLEGRO_BITMAP  *sourceImage, *Image = NULL;
+
+	if (!al_init()) {
+		al_show_native_message_box(display, "Error", "Error", "Failed to initialize allegro!",
+			NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		return 0;
+	}
+
+	if (!al_init_image_addon()) {
+		al_show_native_message_box(display, "Error", "Error", "Failed to initialize al_init_image_addon!",
+			NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		return 0;
+	}
+
+	display = al_create_display(800, 600);
+
+	if (!display) {
+		al_show_native_message_box(display, "Error", "Error", "Failed to initialize display!",
+			NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		return 0;
+	}
+
+	sourceImage = al_load_bitmap("imagen.jpg");
+	Image = al_create_bitmap(50, 50);
+
+	al_draw_scaled_bitmap
+
+
+	if (!image) {
+		al_show_native_message_box(display, "Error", "Error", "Failed to load image!",
+			NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		al_destroy_display(display);
+		return 0;
+	}
+
+	al_draw_bitmap(image, 50, 50, 0);
+
+	al_flip_display();
+	al_rest(2);
+	cin.get();
 	al_destroy_display(display);
 	al_destroy_bitmap(image);
+
+	return 0;
 }
